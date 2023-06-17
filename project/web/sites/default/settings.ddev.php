363406e42e5adb3549997a1b5cf49e97f207f368
Thony353 Cubero <cuberoucr12@gmail.com>
2023-06-17 05:57:04 +0000

    Trabajo con temas

--- project/web/sites/default/settings.ddev.php
+++ project/web/sites/default/settings.ddev.php
@@ -28,7 +28,7 @@ $databases['default']['default'] = array(
   'prefix' => "",
 );
 
-$settings['hash_salt'] = 'WxsWQfeRoUyNaDHDIdAOKhXOqYYSYDUKsbbKRDogNnglrrGVpUHiHvuPEeNNSiGp';
+$settings['hash_salt'] = 'KSgCttMqLwuSkDKPXCvpVTKKiyFFCdXQHunsexzyVfykemzEVKeVOMsdFgHrRgOt';
 
 // This will prevent Drupal from setting read-only permissions on sites/default.
 $settings['skip_permissions_hardening'] = TRUE;
---
+++ project/web/themes/custom/libre/js/libre.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * libre behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.libre = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
